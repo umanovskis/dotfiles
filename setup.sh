c105869ff28316a702bb7a83c5d79fd3ddc48c28
Daniels Umanovskis <daniels@umanovskis.se>
2013-08-27 10:46:17 +0200

    Ensuring plasma-desktop-appletsrc gets a backup

--- setup.sh
+++ setup.sh
@@ -9,4 +9,7 @@ ln -s `pwd`/bash/.bash_aliases "$HOME"/.bash_aliases
 source `readlink -f "$HOME"/.bashrc`
 source `readlink -f "$HOME"/.bash_aliases`
 
+#The Plasma desktop file I definitely always want a backup of
+cp "$HOME"/.kde/share/config/plasma-desktop-appletsrc "$HOME"/.kde/share/config/plasma-desktop-appletsrc.bak || true
+
 cp -r kde/* "$HOME"/.kde
